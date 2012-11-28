3092677ad92b407cae42eb05cbbe25ce1ad390dc
Brett Viren <bv@bnl.gov>
2012-11-28 12:57:46 -0500

    First commit of new lcatr glue.

---
+++ lcatr.tcl
@@ -0,0 +1,18 @@
+# Interface an LCATR software installation to the LCATR job harness
+
+# This file should be sourced by an installations modulefile 
+
+# Register an LCATR modulefile to produce standard environment.  Call
+# with the path to the producer and validator programs relative to the
+# package's installation area
+proc lcatr_package { producer validator } {
+    global env
+
+    set pkgname "$env(LCATR_NAME)/$env(LCATR_VERSION)"
+    set pkgpath "$env(LCATR_INSTALL_AREA)/$pkgname"
+
+    append-path LCATR_LCATR_PKGS "$pkgname"
+
+    setenv LCATR_PRODUCER "$pkgpath/$producer"
+    setenv LCATR_VALIDATOR "$pkgpath/$validator"
+}
